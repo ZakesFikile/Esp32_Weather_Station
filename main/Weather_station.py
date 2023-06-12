4005b1da8d4151403ecca3a7212c027a9617e8da
ZakesFikile <70750701+ZakesFikile@users.noreply.github.com>
2023-06-12 16:32:02 +0200

    Update Weather_station.py
    added funct

--- main/Weather_station.py
+++ main/Weather_station.py
@@ -96,3 +96,107 @@ if __name__ == "__main__":
     main()
 
 
+import network
+import urequests
+from machine import Pin, I2C
+from ssd1306 import SSD1306_I2C
+
+# Default Wi-Fi credentials
+DEFAULT_WIFI_SSID = "your_default_wifi_ssid"
+DEFAULT_WIFI_PASSWORD = "your_default_wifi_password"
+
+# OpenWeatherMap configuration
+OPENWEATHER_API_KEY = "your_openweather_api_key"
+CITY_NAME = "your_city_name"
+OPENWEATHER_API_URL = f"http://api.openweathermap.org/data/2.5/weather?q={CITY_NAME}&appid={OPENWEATHER_API_KEY}"
+
+# Charger I2C address
+CHARGER_I2C_ADDRESS = 0x12
+
+# Display configuration
+SCREEN_WIDTH = 128
+SCREEN_HEIGHT = 64
+i2c = I2C(scl=Pin(22), sda=Pin(21))
+oled = SSD1306_I2C(SCREEN_WIDTH, SCREEN_HEIGHT, i2c)
+
+class WifiConfigServer:
+    def __init__(self):
+        self.ssid = DEFAULT_WIFI_SSID
+        self.password = DEFAULT_WIFI_PASSWORD
+
+    def save_config(self, ssid, password):
+        self.ssid = ssid
+        self.password = password
+        # Save the config to a file or EEPROM
+
+    def restart(self):
+        # Restart the ESP32
+        import machine
+        machine.reset()
+
+class WeatherFetcher:
+    def fetch_weather_data(self):
+        response = urequests.get(OPENWEATHER_API_URL)
+        weather_data = response.json()
+        response.close()
+        return weather_data
+
+    def get_temperature(self, weather_data):
+        return round(weather_data["main"]["temp"] - 273.15, 1)
+
+    def get_humidity(self, weather_data):
+        return weather_data["main"]["humidity"]
+
+    def get_description(self, weather_data):
+        return weather_data["weather"][0]["description"]
+
+class ChargerReader:
+    def __init__(self):
+        self.i2c = I2C(scl=Pin(22), sda=Pin(21))
+
+    def fetch_charger_data(self):
+        self.i2c.start()
+        self.i2c.writeto(CHARGER_I2C_ADDRESS, b"\x01")
+        charger_data = self.i2c.readfrom(CHARGER_I2C_ADDRESS, 4)
+        self.i2c.stop()
+        return charger_data
+
+    def get_voltage(self, charger_data):
+        return charger_data[0] / 10
+
+    def get_current(self, charger_data):
+        return charger_data[1] / 1000
+
+class DisplayManager:
+    def __init__(self):
+        self.oled = SSD1306_I2C(SCREEN_WIDTH, SCREEN_HEIGHT, i2c)
+
+    def clear_screen(self):
+        self.oled.fill(0)
+        self.oled.show()
+
+    def display_text(self, text, x, y):
+        self.oled.text(text, x, y)
+        self.oled.show()
+
+# Initialize Wi-Fi config server
+wifi_config_server = WifiConfigServer()
+
+# Initialize weather fetcher, charger reader, and display manager
+weather_fetcher = WeatherFetcher()
+charger_reader = ChargerReader()
+display_manager = DisplayManager()
+
+# Check if Wi-Fi credentials are saved
+if wifi_config_server.ssid == DEFAULT_WIFI_SSID and wifi_config_server.password == DEFAULT_WIFI_PASSWORD:
+    display_manager.clear_screen()
+    display_manager.display_text("No saved Wi-Fi", 0, 0)
+    display_manager.display_text("credentials found.", 0, 16)
+
+else:
+    # Connect to Wi-Fi
+    display_manager.clear_screen()
+    display_manager.display_text("Connecting
+
+
+
